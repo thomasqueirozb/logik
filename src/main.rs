28b32622d91f5e7a3893079dc65bbef40a4048e7
Thomas Queiroz <thomasqueirozb@gmail.com>
2021-03-17 10:33:07 -0300

    v1.1.0: add operations and comments + refactor + tests
    Fully refactor tokenizer and parser
    Add tests.
    Add multiplication and division operations.
    Remove InitialToken.
    Temporarily disable structopt so input can start with "-"

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,26 @@ mod operator;
 
 use operator::Op;
 
-use anyhow::bail;
-use anyhow::Result;
-use structopt::StructOpt;
+use anyhow::{bail, Context, Result};
+// use structopt::StructOpt;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub enum Token {
     Number(i64),
     Op(Op),
 }
 
-#[derive(Debug)]
-pub enum InitialToken {
-    MaybeNumber(String),
-    Op(Op),
-}
-
+/*
 /// Logik
 #[derive(Debug, StructOpt)]
 #[structopt(author, about)]
 /// Simple command line calculator
 struct Opt {
     /// Input string
+    #[structopt(short, long)]
     input: String,
 }
+*/
 
 fn main() {
     if let Err(e) = run() {
@@ -34,54 +30,116 @@ fn main() {
     }
 }
 
-fn run() -> Result<()> {
-    let opt: Opt = Opt::from_args();
-
-    let mut tokens: Vec<InitialToken> = vec![];
+fn tokenize(input: String) -> Result<Vec<Token>> {
+    let mut tokens: Vec<Token> = vec![];
 
     let mut buffer = String::new();
 
-    let tokens_push_buf = |buffer: &mut String, tokens: &mut Vec<InitialToken>| {
-        if !buffer.is_empty() {
-            tokens.push(InitialToken::MaybeNumber(buffer.clone()));
-            buffer.clear();
-        }
+    #[derive(PartialEq, Copy, Clone, Debug)]
+    enum TokenizerState {
+        None,
+        Number,
+        Op,
+        Comment,
     };
 
-    for c in opt.input.chars() {
-        if !c.is_ascii() {
-            continue;
+    let mut state = TokenizerState::None;
+
+    for (_pos, c) in input.chars().chain(std::iter::once('\0')).enumerate() {
+        let prev_state = state;
+        if (c.is_whitespace() || c == '\0') && state != TokenizerState::Comment {
+            state = TokenizerState::None;
+        } else {
+            match state {
+                TokenizerState::None => {
+                    if c.is_numeric() {
+                        state = TokenizerState::Number;
+                    } else if Op::is_op(c) {
+                        state = TokenizerState::Op;
+                    } else {
+                        bail!("Unparsable char '{}'", c);
+                    }
+                }
+                TokenizerState::Number => {
+                    if !c.is_numeric() {
+                        if Op::is_op(c) {
+                            state = TokenizerState::Op;
+                        }
+                    }
+                }
+                TokenizerState::Op => {
+                    if !Op::is_op(c) {
+                        state = TokenizerState::Number;
+                    } else {
+                        if let Some(lc) = buffer.chars().last() {
+                            if lc == '/' && c == '*' {
+                                buffer.pop();
+                                state = TokenizerState::Comment;
+                            }
+                        }
+                    }
+                }
+                TokenizerState::Comment => {
+                    match c {
+                        '*' => {
+                            buffer.clear(); //FIXME
+                            buffer.push(c);
+                        }
+                        '/' => {
+                            if !buffer.is_empty() {
+                                state = TokenizerState::None
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            // println!("c {}, state {:?}", c, state);
         }
 
-        if let Some(op) = Op::from_char(c) {
-            tokens_push_buf(&mut buffer, &mut tokens);
-            tokens.push(InitialToken::Op(op));
-        } else {
-            if c.is_ascii_whitespace() {
-                tokens_push_buf(&mut buffer, &mut tokens);
-            } else {
-                buffer.push(c);
+        if prev_state != state {
+            match prev_state {
+                TokenizerState::None | TokenizerState::Comment => {
+                    buffer.clear();
+                }
+                TokenizerState::Op => {
+                    if buffer.len() > 1 {
+                        bail!("Multiple operators together");
+                    } else if buffer.len() != 0 {
+                        tokens.push(Token::Op(
+                            Op::from_char(buffer.chars().next().unwrap()).context("Op conv")?,
+                        ));
+
+                        buffer.clear();
+                    }
+                }
+
+                TokenizerState::Number => {
+                    if buffer.len() != 0 {
+                        match buffer.parse::<i64>() {
+                            Ok(num) => tokens.push(Token::Number(num)),
+                            Err(e) => bail!("Could not convert \"{}\" to i64 - ({})", buffer, e),
+                        }
+
+                        buffer.clear();
+                    }
+                }
             }
+        }
+        match state {
+            TokenizerState::None | TokenizerState::Comment => {}
+            _ => buffer.push(c),
         }
     }
 
-    tokens_push_buf(&mut buffer, &mut tokens);
+    if state == TokenizerState::Comment {
+        bail!("Unterminated comment");
+    }
+    Ok(tokens)
+}
 
-    let tokens: Vec<Token> = {
-        let mut nt = vec![];
-        for tk in tokens {
-            let v = match tk {
-                InitialToken::Op(op) => Token::Op(op),
-                InitialToken::MaybeNumber(mb_num) => match mb_num.parse::<i64>() {
-                    Ok(num) => Token::Number(num),
-                    Err(e) => bail!("Could not convert \"{}\" to i64 - ({})", mb_num, e),
-                },
-            };
-            nt.push(v);
-        }
-        nt
-    };
-
+fn parse(tokens: Vec<Token>) -> Result<i64> {
+    // println!("tokens {:?}", tokens);
     if let Some(_first) = tokens.get(0) {
         /*
         if let Token::Op(op) = first {
@@ -91,36 +149,155 @@ fn run() -> Result<()> {
         }
         */
 
-        let mut counter = match &tokens[0] {
-            Token::Op(op) => {
-                bail!("Cannot start with \"{}\"", op);
+        let mut nt = tokens.clone();
+
+        let get_num = |nt: &mut Vec<Token>, i: usize| -> Result<i64> {
+            match nt.get(i).context("Missing token")? {
+                Token::Op(op) => bail!("Expected number found operator \"{}\"", op),
+                Token::Number(num) => Ok(*num),
             }
-            Token::Number(num) => *num,
         };
 
-        for i in (1..tokens.len()).filter(|x| x % 2 == 1) {
-            let tk = &tokens[i];
-            counter = match tk {
-                Token::Op(op) => {
-                    if (i + 1) >= tokens.len() {
-                        bail!("Expected something after \"{}\"", op);
-                    }
-
-                    let tk_after = &tokens[i + 1];
-                    match tk_after {
-                        Token::Op(after_op) => {
-                            bail!("Expected number found operator \"{}\"", after_op)
-                        }
-                        Token::Number(num) => op.execute(counter, *num),
-                    }
-                }
+        let get_op = |nt: &mut Vec<Token>, i: usize| -> Result<Op> {
+            match nt.get(i).context("Missing token")? {
+                Token::Op(op) => Ok(*op),
                 Token::Number(num) => bail!("Expected operator found number \"{}\"", num),
-            };
+            }
+        };
+
+        let mut has_changed = true;
+        while has_changed {
+            has_changed = false;
+
+            let mut i = 0;
+            while i < nt.len() {
+                let num_0 = get_num(&mut nt, i)?;
+
+                if i + 1 >= nt.len() {
+                    break;
+                }
+
+                let op_1 = get_op(&mut nt, i + 1)?;
+
+                if op_1 == Op::Add || op_1 == Op::Sub {
+                    i += 2;
+                } else {
+                    let num_2 = get_num(&mut nt, i + 2)?;
+
+                    nt.remove(i);
+                    nt.remove(i);
+                    nt.remove(i);
+                    nt.insert(i, Token::Number(op_1.execute(num_0, num_2)));
+                    has_changed = true;
+                }
+            }
         }
-        println!("{}", counter);
+
+        let mut counter = get_num(&mut nt, 0)?;
+
+        let mut i = 1;
+        while i < nt.len() {
+            let op = get_op(&mut nt, i)?;
+
+            if i + 1 >= nt.len() {
+                bail!("Expected number after operator");
+            }
+
+            let num = get_num(&mut nt, i + 1)?;
+            counter = op.execute(counter, num);
+            i += 2;
+        }
+
+        Ok(counter)
     } else {
         bail!("Empty string");
     }
+}
+
+pub fn eval<T>(input: T) -> Result<i64>
+where
+    T: Into<String>,
+{
+    let tokens = tokenize(input.into())?;
+
+    let result = parse(tokens)?;
+    Ok(result)
+}
+
+fn run() -> Result<()> {
+    // let opt: Opt = Opt::from_args();
+    // let input = opt.input;
+    let mut args = std::env::args();
+    args.next().context("Missing path???")?;
+    let input = args.next().context("Missing input")?;
+    if args.next().is_some() {
+        bail!("Too many arguments expected only one");
+    }
+    println!("{}", eval(input)?);
 
     Ok(())
+}
+
+#[test]
+fn comment() {
+    assert_eq!(eval("/* a */ 1 /* b */").unwrap(), 1);
+    assert_eq!(eval("/* /* 3 */ 1 /* b */").unwrap(), 1);
+    assert_eq!(eval("/* /**/ 2+1 /* b */").unwrap(), 3);
+    assert_eq!(eval("1-/*/**/ 2+1 /* b */").unwrap(), 0);
+}
+
+#[test]
+fn sum_sub() {
+    assert_eq!(eval("1  -   04 ").unwrap(), -3);
+    assert_eq!(eval("11-4").unwrap(), 7);
+    assert_eq!(eval("11-4 + 22 -23").unwrap(), 6);
+    assert_eq!(eval("1  +   4 ").unwrap(), 5);
+    assert_eq!(eval("1 + 40 ").unwrap(), 41);
+}
+
+#[test]
+fn div_mul() {
+    assert_eq!(eval("81/ 9 + 3").unwrap(), 12);
+    // assert_eq!(eval("4/2").unwrap(), 2);
+    // assert_eq!(eval("4/2 + 3").unwrap(), 5);
+    // assert_eq!(eval("3 + 4/2").unwrap(), 5);
+    // assert_eq!(eval("3*5 + 10").unwrap(), 25);
+    // assert_eq!(eval("10 + 3*5").unwrap(), 25);
+}
+
+#[test]
+fn errors() {
+    assert_eq!(
+        eval("1 a  -   04 ").unwrap_err().to_string(),
+        "Unparsable char 'a'"
+    );
+    assert_eq!(
+        eval("1a1-4").unwrap_err().to_string(),
+        "Could not convert \"1a1\" to i64 - (invalid digit found in string)"
+    );
+    assert_eq!(
+        eval("11-4/* + 22 -23").unwrap_err().to_string(),
+        "Unterminated comment"
+    );
+    assert_eq!(
+        eval("3- 3 /* a").unwrap_err().to_string(),
+        "Unterminated comment"
+    );
+
+    assert_eq!(
+        eval("3+ /* a */").unwrap_err().to_string(),
+        "Expected number after operator"
+    );
+
+    assert_eq!(
+        eval("-3 /* a */").unwrap_err().to_string(),
+        "Expected number found operator \"-\""
+    );
+
+    assert_eq!(
+        eval("3+ /* a */-").unwrap_err().to_string(),
+        "Expected number found operator \"-\""
+    );
+
+    assert_eq!(eval("/* */").unwrap_err().to_string(), "Empty string");
 }
--- src/operator.rs
+++ src/operator.rs
@@ -1,10 +1,11 @@
+use std::convert::TryFrom;
 use std::fmt;
 use std::ops;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Op {
-    // Mul,
-    // Div,
+    Mul,
+    Div,
     Add,
     Sub,
 }
@@ -13,26 +14,51 @@ impl Op {
     pub fn from_char(c: char) -> Option<Self> {
         use Op::*;
         match c {
-            // '*' => Some(Mul),
-            // '/' => Some(Div),
+            '*' => Some(Mul),
+            '/' => Some(Div),
             '+' => Some(Add),
             '-' => Some(Sub),
             _ => None,
         }
     }
 
+    pub fn is_op(c: char) -> bool {
+        match c {
+            '*' => true,
+            '/' => true,
+            '+' => true,
+            '-' => true,
+            _ => false,
+        }
+    }
+
     pub fn execute<T>(self, lhs: T, rhs: T) -> T
     where
-        // T: ops::Mul<Output = T>,
-        // T: ops::Div<Output = T>,
+        T: ops::Mul<Output = T>,
+        T: ops::Div<Output = T>,
         T: ops::Add<Output = T>,
         T: ops::Sub<Output = T>,
     {
         match self {
-            // Op::Mul => lhs * rhs,
-            // Op::Div => lhs / rhs,
+            Op::Mul => lhs * rhs,
+            Op::Div => lhs / rhs,
             Op::Add => lhs + rhs,
             Op::Sub => lhs - rhs,
+        }
+    }
+}
+
+impl TryFrom<char> for Op {
+    type Error = &'static str;
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        use Op::*;
+        match c {
+            '*' => Ok(Mul),
+            '/' => Ok(Div),
+            '+' => Ok(Add),
+            '-' => Ok(Sub),
+            _ => Err("Op: Char not in \"*/+-\""),
         }
     }
 }
@@ -43,8 +69,8 @@ impl fmt::Display for Op {
             f,
             "{}",
             match self {
-                // Op::Mul => '*',
-                // Op::Div => '/',
+                Op::Mul => '*',
+                Op::Div => '/',
                 Op::Add => '+',
                 Op::Sub => '-',
             },
